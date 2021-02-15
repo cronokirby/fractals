3902392847cc8b685f265af1787615dc76d40e47
Lúcás Meier <cronokirby@gmail.com>
2021-02-15 01:33:21 +0100

    Remove depth testing
    Not really necessary, given the nature of our shader

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -64,8 +64,6 @@ function initBuffers(gl: WebGLRenderingContext) {
 function drawScene(gl: WebGLRenderingContext, programInfo: any, buffers: { position: WebGLBuffer }) {
   gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
   gl.clearDepth(1.0);                 // Clear everything
-  gl.enable(gl.DEPTH_TEST);           // Enable depth testing
-  gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
 
   // Clear the canvas before we start drawing on it.
 
