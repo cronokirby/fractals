8e2119d3fd7b95078f07f50e3cb2900b950744e5
Lúcás Meier <cronokirby@gmail.com>
2021-02-15 09:56:44 +0100

    Extract out iterate function

--- src/fsSource.ts
+++ src/fsSource.ts
@@ -3,29 +3,33 @@ const fragmentShader = `
   precision mediump float;
   #endif
 
-
   uniform vec2 uResolution;
 
   #define N 64.
   #define B 4.
 
-  void main() {
-    vec2 uv = (2. * gl_FragCoord.xy - uResolution) / uResolution.y;
-
+  float iterate(vec2 p) {
     vec2 z = vec2(0);
-    vec2 c = uv;
-
+    vec2 c = p;
     float i;
     for (float j = 0.; j < N; j++) {
       i = j;
       z = mat2(z, -z.y, z.x) * z + c;
-      if (dot(z, z) > B * B) break;
+      if (dot(z, z) > B * B) {
+        break;
+      }
     }
-    if (i == N - 1.) {
-      i = 0.;
-    }
+    return i + 1.;
+  }
 
-	  gl_FragColor = vec4(vec3(i / N), 1.0);
+  void main() {
+    vec2 uv = 1.2 * (2. * gl_FragCoord.xy - uResolution) / uResolution.y - vec2(.4, 0.);
+
+    float n = iterate(uv) / N;
+    if (n == 1.) {
+      n = 0.;
+    }
+	  gl_FragColor = vec4(vec3(n), 1.0);
   }
 `
 export default fragmentShader;
