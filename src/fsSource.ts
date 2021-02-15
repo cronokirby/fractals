bdc77f4be324bfc85f59363d8e60aa5169fc223d
Lúcás Meier <cronokirby@gmail.com>
2021-02-15 02:07:38 +0100

    Define fractal

--- src/fsSource.ts
+++ src/fsSource.ts
@@ -3,11 +3,29 @@ const fragmentShader = `
   precision mediump float;
   #endif
 
+
   uniform vec2 uResolution;
 
+  #define N 64.
+  #define B 4.
+
   void main() {
-    vec2 st = gl_FragCoord.xy / uResolution;
-	  gl_FragColor = vec4(st.x, st.y, 0.0, 1.0);
+    vec2 uv = (2. * gl_FragCoord.xy - uResolution) / uResolution.y;
+
+    vec2 z = vec2(0);
+    vec2 c = uv;
+
+    float i;
+    for (float j = 0.; j < N; j++) {
+      i = j;
+      z = mat2(z, -z.y, z.x) * z + c;
+      if (dot(z, z) > B * B) break;
+    }
+    if (i == N - 1.) {
+      i = 0.;
+    }
+
+	  gl_FragColor = vec4(vec3(i / N), 1.0);
   }
 `
 export default fragmentShader;
